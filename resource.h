7430f871acbc60da91f57d4143273905bc9e41b7
Anup Ghimire <146419410+aancur2002@users.noreply.github.com>
2026-05-23 03:10:31 +0545

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,6 +1,9 @@
-#pragma once
+#ifndef AMCAP_RESOURCE_H
+#define AMCAP_RESOURCE_H
 
 #define IDR_MENU               101
 #define IDI_APP_ICON           102
 #define ID_FILE_EXIT           40001
 #define ID_VIEW_FULLSCREEN     40002
+
+#endif
